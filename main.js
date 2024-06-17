f5b4c7be1d6795b65fac60fba5bf0e8756f95ed8
puterifatiin <168213790+puterifatiin@users.noreply.github.com>
2024-06-18 01:35:41 +0800

    18/6 (done home, member, developed algo, references)

--- main.js
+++ main.js
@@ -149,3 +149,13 @@
 
 })(jQuery);
 
+// Code expand
+function toggleCode(id) {
+    var element = document.getElementById(id);
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
+}
+
